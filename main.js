5b3796d1c8d5053931d93e7be3fd317d67575500
drfstrange <136354531+drfstrange@users.noreply.github.com>
2025-06-06 19:25:27 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-import * as THREE from './libs/three.module.js';
-import { OrbitControls } from './libs/OrbitControls.js';
-import { GLTFLoader } from './libs/GLTFLoader.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.174.0/build/three.module.js';
+import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.174.0/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.174.0/examples/jsm/loaders/GLTFLoader.js';
+
 
 
 const scene = new THREE.Scene();
